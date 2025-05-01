715a08e798873ff90906472291fa1f17617c5ca9
EdenWijaya <eden.122140187@student.itera.ac.id>
2025-05-02 01:15:15 +0700

    add welcome 1

---
+++ uneeds/lib/utils/color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color blueColor = const Color(0xFF00569E);
+Color fontGrayColor = const Color(0xFF888888);
+Color iconGrayColor = const Color(0xFFCCCCCC);
---
+++ uneeds/lib/views/onboarding.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:uneeds/utils/color.dart';
+
+class OnboardingView extends StatefulWidget {
+  const OnboardingView({super.key});
+
+  @override
+  State<OnboardingView> createState() => _OnboardingViewState();
+}
+
+class _OnboardingViewState extends State<OnboardingView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.asset("assets/gambar/welcome-1.png"),
+
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Text(
+                    "Selamat Datang di\nUNEEDS",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontFamily: "InterBold", fontSize: 24),
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: "InterMedium",
+                      fontSize: 14,
+                      color: fontGrayColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: GestureDetector(
+              onTap: () {
+                print("lanjut");
+              },
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(vertical: 13.5),
+                decoration: BoxDecoration(
+                  color: blueColor,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text(
+                  "Lanjut",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: "InterSemiBold",
+                    fontSize: 14,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: GestureDetector(
+              onTap: () {
+                print("lewati");
+              },
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(vertical: 13.5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text(
+                  "Lewati",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: "InterSemiBold",
+                    fontSize: 14,
+                    color: blueColor,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
