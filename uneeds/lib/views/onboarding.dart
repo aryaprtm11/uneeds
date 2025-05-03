0f10fda66743e59540ad00deefe22e3cb2010f62
EdenWijaya <eden.122140187@student.itera.ac.id>
2025-05-03 16:47:24 +0700

    add slider

--- uneeds/lib/views/onboarding.dart
+++ uneeds/lib/views/onboarding.dart
@@ -14,87 +14,106 @@ class _OnboardingViewState extends State<OnboardingView> {
     return Scaffold(
       body: Column(
         children: [
-          Image.asset("assets/gambar/welcome-1.png"),
+          Expanded(
+            child: PageView.builder(
+              itemBuilder: (_, i) {
+                return Column(
+                  children: [
+                    Image.asset("assets/gambar/welcome-${i + 1}.png"),
 
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20.0),
-                  child: Text(
-                    "Selamat Datang di\nUNEEDS",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontFamily: "InterBold", fontSize: 24),
-                  ),
-                ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 20.0),
+                            child: Text(
+                              "Selamat Datang di\nUNEEDS",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: "InterBold",
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
 
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Text(
-                    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: "InterMedium",
-                      fontSize: 14,
-                      color: fontGrayColor,
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0,
+                            ),
+                            child: Text(
+                              "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontFamily: "InterMedium",
+                                fontSize: 14,
+                                color: fontGrayColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+
+          Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: GestureDetector(
+                  onTap: () {
+                    print("lanjut");
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.symmetric(vertical: 13.5),
+                    decoration: BoxDecoration(
+                      color: blueColor,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Text(
+                      "Lanjut",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: "InterSemiBold",
+                        fontSize: 14,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-              ],
-            ),
-          ),
+              ),
 
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: GestureDetector(
-              onTap: () {
-                print("lanjut");
-              },
-              child: Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(vertical: 13.5),
-                decoration: BoxDecoration(
-                  color: blueColor,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Text(
-                  "Lanjut",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: "InterSemiBold",
-                    fontSize: 14,
-                    color: Colors.white,
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: GestureDetector(
+                  onTap: () {
+                    print("lewati");
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.symmetric(vertical: 13.5),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Text(
+                      "Lewati",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: "InterSemiBold",
+                        fontSize: 14,
+                        color: blueColor,
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ),
-
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: GestureDetector(
-              onTap: () {
-                print("lewati");
-              },
-              child: Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(vertical: 13.5),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Text(
-                  "Lewati",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: "InterSemiBold",
-                    fontSize: 14,
-                    color: blueColor,
-                  ),
-                ),
-              ),
-            ),
+            ],
           ),
         ],
       ),
